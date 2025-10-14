85d9c91c302cdbac8cf04c7b74ff0a31f09cf279
danmv24 <danmn119@gmail.com>
2025-10-14 22:22:50 +0300

    added a foreign key to the employees table to link to the positions table

--- src/main/resources/db/changelog/changes/create_table_employees.sql
+++ src/main/resources/db/changelog/changes/create_table_employees.sql
@@ -1,8 +1,10 @@
 CREATE TABLE "employees" (
-    id SERIAL PRIMARY KEY,
+    employee_id SERIAL PRIMARY KEY,
     surname VARCHAR(60) NOT NULL,
     name VARCHAR(30) NOT NULL,
     patronymic VARCHAR(70) NOT NULL,
+    position_id INT,
     salary DECIMAL(8, 2),
-    hire_date TIMESTAMP
+    hire_date TIMESTAMP,
+    FOREIGN KEY (position_id) REFERENCES positions(position_id)
 );
