4385f7e8d53df310187dff08bc80a3ad5abd22de
Danis Mingaleev <danmn119@gmail.com>
2025-10-14 20:04:38 +0300

    initial commit

---
+++ src/main/java/com/agropro/AgroPro/AgroProApplication.java
@@ -0,0 +1,13 @@
+package com.agropro.AgroPro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgroProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgroProApplication.class, args);
+	}
+
+}
