9c218539d229277329a1ffe9154623f920cdd89d
danmv24 <danmn119@gmail.com>
2025-11-06 18:47:49 +0300

    the position model that contains information about it

---
+++ src/main/java/com/agropro/AgroPro/model/Position.java
@@ -0,0 +1,19 @@
+package com.agropro.AgroPro.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.relational.core.mapping.Column;
+import org.springframework.data.relational.core.mapping.Table;
+
+@Table(name = "positions")
+@Getter
+@Setter
+public class Position {
+
+    @Column("position_id")
+    private Long positionId;
+
+    @Column("position_name")
+    private String positionName;
+
+}
