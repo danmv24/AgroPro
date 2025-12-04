eb3d4af21f4f90ab35349fb0cce47446a95b552f
danmv24 <danmn119@gmail.com>
2025-12-04 16:58:35 +0300

    some changes

--- src/main/java/com/agropro/AgroPro/exception/PositionNotFoundException.java
+++ src/main/java/com/agropro/AgroPro/exception/PositionNotFoundException.java
@@ -1,15 +1,16 @@
 package com.agropro.AgroPro.exception;
 
+import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
+@Getter
 public class PositionNotFoundException extends RuntimeException {
 
-    private HttpStatus status;
-    private String message;
+    private final HttpStatus status;
 
     public PositionNotFoundException(HttpStatus status, String message) {
         super(message);
         this.status = status;
-        this.message = message;
     }
+
 }
