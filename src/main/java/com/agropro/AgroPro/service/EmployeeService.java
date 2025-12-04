45cc9d0b2a206b2750840ad4b5af7d9111f6cd5f
danmv24 <danmn119@gmail.com>
2025-12-04 17:18:32 +0300

    three new methods: getHourlyEmployees returns the EmployeeBasicInfo list for the form; addWorkRecord saves the record to the database; getAllWorkRecords outputs all records

--- src/main/java/com/agropro/AgroPro/service/EmployeeService.java
+++ src/main/java/com/agropro/AgroPro/service/EmployeeService.java
@@ -1,7 +1,10 @@
 package com.agropro.AgroPro.service;
 
 import com.agropro.AgroPro.form.EmployeeForm;
+import com.agropro.AgroPro.form.WorkRecordForm;
+import com.agropro.AgroPro.view.EmployeeBasicInfo;
 import com.agropro.AgroPro.view.EmployeeView;
+import com.agropro.AgroPro.view.WorkRecordView;
 
 import java.util.List;
 
@@ -10,5 +13,11 @@ public interface EmployeeService {
     void addEmployee(EmployeeForm employeeForm);
 
     List<EmployeeView> getEmployees();
+
+    List<EmployeeBasicInfo> getHourlyEmployees();
+
+    void addWorkRecord(WorkRecordForm workRecordForm);
+
+    List<WorkRecordView> getAllWorkRecords();
 
 }
--- src/main/java/com/agropro/AgroPro/service/impl/DefaultEmployeeService.java
+++ src/main/java/com/agropro/AgroPro/service/impl/DefaultEmployeeService.java
@@ -1,14 +1,20 @@
 package com.agropro.AgroPro.service.impl;
 
+import com.agropro.AgroPro.exception.EmployeeNotFoundException;
 import com.agropro.AgroPro.exception.PositionNotFoundException;
 import com.agropro.AgroPro.form.EmployeeForm;
+import com.agropro.AgroPro.form.WorkRecordForm;
 import com.agropro.AgroPro.mapper.EmployeeMapper;
+import com.agropro.AgroPro.mapper.WorkRecordMapper;
 import com.agropro.AgroPro.model.Employee;
 import com.agropro.AgroPro.model.Position;
 import com.agropro.AgroPro.repository.EmployeeRepository;
 import com.agropro.AgroPro.repository.PositionRepository;
+import com.agropro.AgroPro.repository.WorkRecordRepository;
 import com.agropro.AgroPro.service.EmployeeService;
+import com.agropro.AgroPro.view.EmployeeBasicInfo;
 import com.agropro.AgroPro.view.EmployeeView;
+import com.agropro.AgroPro.view.WorkRecordView;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
@@ -21,12 +27,13 @@ public class DefaultEmployeeService implements EmployeeService {
 
     private final EmployeeRepository employeeRepository;
     private final PositionRepository positionRepository;
+    private final WorkRecordRepository workRecordRepository;
 
     @Override
     public void addEmployee(EmployeeForm employeeForm) {
         Long positionId = positionRepository.findByPositionName(employeeForm.getPosition())
                 .map(Position::getPositionId)
-                .orElseThrow(() -> new PositionNotFoundException(HttpStatus.NOT_FOUND, "Position not found: " + employeeForm.getPosition()));
+                .orElseThrow(() -> new PositionNotFoundException(HttpStatus.NOT_FOUND, "Должность не найдена: " + employeeForm.getPosition()));
 
         employeeRepository.save(EmployeeMapper.toModel(employeeForm, positionId));
     }
@@ -38,9 +45,30 @@ public class DefaultEmployeeService implements EmployeeService {
         return employees.stream()
                 .map(employee -> {
                     Position position = positionRepository.findByPositionId(employee.getPositionId())
-                            .orElseThrow(() -> new PositionNotFoundException(HttpStatus.NOT_FOUND, "Position not found: " + employee.getPositionId()));
+                            .orElseThrow(() -> new PositionNotFoundException(HttpStatus.NOT_FOUND, "Не найдена должность с id:" + employee.getPositionId()));
                     return EmployeeMapper.toView(employee, position.getPositionName());
                 })
                 .toList();
     }
+
+    @Override
+    public List<EmployeeBasicInfo> getHourlyEmployees() {
+        return employeeRepository.findEmployeesWherePaymentTypeIsHourly();
+    }
+
+    @Override
+    public void addWorkRecord(WorkRecordForm workRecordForm) {
+        if (!employeeRepository.existByEmployeeId(workRecordForm.getEmployeeId())) {
+            throw new EmployeeNotFoundException(HttpStatus.NOT_FOUND, "Не найден сотрудник с id: " + workRecordForm.getEmployeeId());
+        }
+
+        workRecordRepository.save(WorkRecordMapper.toModel(workRecordForm));
+    }
+
+    @Override
+    public List<WorkRecordView> getAllWorkRecords() {
+        return workRecordRepository.findAllWorkRecords();
+    }
+
+
 }
