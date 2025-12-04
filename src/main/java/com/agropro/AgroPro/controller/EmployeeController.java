0adad3e3dd3959be386b10e9a5115e916d0d34a4
danmv24 <danmn119@gmail.com>
2025-12-04 17:16:27 +0300

    three new methods: showWorkRecordForm outputs a form; addWorkRecord to process the form; getListWorkRecords outputs a page with information about the work

--- src/main/java/com/agropro/AgroPro/controller/EmployeeController.java
+++ src/main/java/com/agropro/AgroPro/controller/EmployeeController.java
@@ -1,9 +1,11 @@
 package com.agropro.AgroPro.controller;
 
 import com.agropro.AgroPro.form.EmployeeForm;
+import com.agropro.AgroPro.form.WorkRecordForm;
 import com.agropro.AgroPro.repository.PositionRepository;
 import com.agropro.AgroPro.service.EmployeeService;
 import com.agropro.AgroPro.view.EmployeeView;
+import com.agropro.AgroPro.view.WorkRecordView;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -41,5 +43,27 @@ public class EmployeeController {
         List<EmployeeView> employeeViews = employeeService.getEmployees();
         model.addAttribute("employeeView", employeeViews);
         return "employees/employee_list";
+    }
+
+    @GetMapping("/work-time/add")
+    public String showWorkRecordForm(Model model) {
+        model.addAttribute("workTimeForm", new WorkRecordForm());
+        model.addAttribute("employees", employeeService.getHourlyEmployees());
+
+        return "employees/worktime_add";
+    }
+
+    @PostMapping("/work-time/add")
+    public ResponseEntity<Map<String, String>> addWorkRecord(@RequestBody WorkRecordForm workTimeForm) {
+        employeeService.addWorkRecord(workTimeForm);
+        return ResponseEntity.ok(Map.of("status", "success", "message", "Сотрудник успешно добавлен"));
+    }
+
+    @GetMapping("/work-time/list")
+    public String getListWorkRecords(Model model) {
+        List<WorkRecordView> workRecordViews = employeeService.getAllWorkRecords();
+        model.addAttribute("workRecords", workRecordViews);
+
+        return "employees/worktime_list";
     }
 }
