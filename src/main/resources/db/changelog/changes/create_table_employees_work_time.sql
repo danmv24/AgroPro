aabbb416c3eb07b242549e2fa9cfda9b7a22bc0a
danmv24 <danmn119@gmail.com>
2025-12-16 13:25:47 +0300

    added a foreign key

--- src/main/resources/db/changelog/changes/create_table_employees_work_time.sql
+++ src/main/resources/db/changelog/changes/create_table_employees_work_time.sql
@@ -1,6 +1,7 @@
 CREATE TABLE "employees_work_time" (
     id SERIAL PRIMARY KEY,
-    employee_id INT,
+    employee_id INT NOT NULL,
     work_date DATE,
-    hours_worked DECIMAL(5, 2) NOT NULL CHECK (hours_worked >= 0)
+    hours_worked DECIMAL(5, 2) NOT NULL CHECK (hours_worked >= 0),
+    FOREIGN KEY (employee_id) REFERENCES employees(employee_id)
 );
