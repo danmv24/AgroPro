0fdf4b680fb6f64f78c9e50b596767b399d3c040
danmv24 <danmn119@gmail.com>
2025-12-17 16:45:50 +0300

    import removed

--- src/main/java/com/agropro/AgroPro/model/FieldPlanting.java
+++ src/main/java/com/agropro/AgroPro/model/FieldPlanting.java
@@ -4,7 +4,6 @@ import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Column;
 import org.springframework.data.relational.core.mapping.Table;
-import org.springframework.data.relational.core.sql.In;
 
 @Table(name = "field_plantings")
 @Getter
