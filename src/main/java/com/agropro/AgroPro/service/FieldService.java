9540169be0cc16ba463bb76990d0e576d52577e5
danmv24 <danmn119@gmail.com>
2025-12-17 17:20:30 +0300

    field service

---
+++ src/main/java/com/agropro/AgroPro/service/FieldService.java
@@ -0,0 +1,9 @@
+package com.agropro.AgroPro.service;
+
+import com.agropro.AgroPro.form.FieldPlantingForm;
+
+public interface FieldService {
+
+    void addFieldPlanting(FieldPlantingForm fieldForm);
+
+}
---
+++ src/main/java/com/agropro/AgroPro/service/impl/DefaultFieldService.java
@@ -0,0 +1,42 @@
+package com.agropro.AgroPro.service.impl;
+
+import com.agropro.AgroPro.exception.CropNotFoundException;
+import com.agropro.AgroPro.exception.FieldNotFoundException;
+import com.agropro.AgroPro.form.FieldPlantingForm;
+import com.agropro.AgroPro.mapper.FieldPlantingMapper;
+import com.agropro.AgroPro.repository.CropRepository;
+import com.agropro.AgroPro.repository.FieldPlantingRepository;
+import com.agropro.AgroPro.repository.FieldRepository;
+import com.agropro.AgroPro.service.FieldService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class DefaultFieldService implements FieldService {
+
+    private final FieldRepository fieldRepository;
+    private final CropRepository cropRepository;
+    private final FieldPlantingRepository fieldPlantingRepository;
+
+    @Override
+    public void addFieldPlanting(FieldPlantingForm fieldPlantingForm) {
+        validateFieldExists(fieldPlantingForm.getFieldId());
+        validateCropExists(fieldPlantingForm.getCropId());
+
+        fieldPlantingRepository.save(FieldPlantingMapper.toModel(fieldPlantingForm));
+    }
+
+    private void validateFieldExists(Long fieldId) {
+        if (!fieldRepository.existsByFieldId(fieldId)) {
+            throw new FieldNotFoundException(HttpStatus.NOT_FOUND, fieldId);
+        }
+    }
+
+    private void validateCropExists(Long cropId) {
+        if (!cropRepository.existsByCropId(cropId)) {
+            throw new CropNotFoundException(HttpStatus.NOT_FOUND, cropId);
+        }
+    }
+}
