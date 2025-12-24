82fe7d234a944df9db1585b0af3c67b4f7c843db
danmv24 <danmn119@gmail.com>
2025-12-24 12:42:01 +0300

    add endpoints for field-planting list view and AJAX data fetching

--- src/main/java/com/agropro/AgroPro/controller/FieldPlantingController.java
+++ src/main/java/com/agropro/AgroPro/controller/FieldPlantingController.java
@@ -4,15 +4,14 @@ import com.agropro.AgroPro.form.FieldPlantingForm;
 import com.agropro.AgroPro.repository.CropRepository;
 import com.agropro.AgroPro.repository.FieldRepository;
 import com.agropro.AgroPro.service.FieldService;
+import com.agropro.AgroPro.view.FieldWithCurrentCropView;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Map;
 
 @Controller
@@ -36,6 +35,17 @@ public class FieldPlantingController {
     public ResponseEntity<Map<String, String>> addFieldPlanting(@RequestBody FieldPlantingForm fieldForm) {
         fieldService.addFieldPlanting(fieldForm);
         return ResponseEntity.ok(Map.of("status", "success", "message", "Успешно"));
+    }
+
+    @GetMapping("/list")
+    public String getAllPlantings(Model model) {
+        return "fields/field_plantings_list";
+    }
+
+    @GetMapping("/data-json")
+    @ResponseBody
+    public List<FieldWithCurrentCropView> getFieldPlantingsDataJson(@RequestParam(defaultValue = "2025") Integer year) {
+        return fieldService.getFieldsWithCropByYear(year);
     }
 
 }
