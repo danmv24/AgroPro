76ca2ebdad94d10a859d7fb0d33a5c1c13499ccc
danmv24 <danmn119@gmail.com>
2026-01-08 16:33:52 +0300

    Add DefaultEquipmentService as the concrete implementation of EquipmentService interface. This service will contain business logic for equipment management operations.

---
+++ src/main/java/com/agropro/AgroPro/service/impl/DefaultEquipmentService.java
@@ -0,0 +1,13 @@
+package com.agropro.AgroPro.service.impl;
+
+import com.agropro.AgroPro.service.EquipmentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class DefaultEquipmentService implements EquipmentService {
+
+
+
+}
