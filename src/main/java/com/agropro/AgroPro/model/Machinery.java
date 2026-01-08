2273648d76bddb172fdbd6a0ff0d3ae9da2b9d1e
danmv24 <danmn119@gmail.com>
2026-01-08 16:15:05 +0300

    add Machinery model

---
+++ src/main/java/com/agropro/AgroPro/model/Machinery.java
@@ -0,0 +1,35 @@
+package com.agropro.AgroPro.model;
+
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
+import org.springframework.data.relational.core.mapping.Table;
+
+@Table(name = "machineries")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Machinery {
+
+    @Id
+    @Column("machinery_id")
+    private String machineryId;
+
+    @Column("machinery_name")
+    private String machineryName;
+
+    @Column("machinery_type_id")
+    private Long machineryTypeId;
+
+    @Column("inventory_number")
+    private Integer inventoryNumber;
+
+    @Column("licence_plate")
+    private String licencePlate;
+
+    @Column("status_id")
+    private Long statusId;
+
+}
