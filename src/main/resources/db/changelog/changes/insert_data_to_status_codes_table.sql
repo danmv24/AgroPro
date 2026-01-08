34d4c408aa85da6ba28dce87cad75bd92d48368d
danmv24 <danmn119@gmail.com>
2026-01-08 16:09:28 +0300

    sql script for inserting data into the status_codes table

---
+++ src/main/resources/db/changelog/changes/insert_data_to_status_codes_table.sql
@@ -0,0 +1,4 @@
+INSERT INTO status_codes(status_code, display_name) VALUES ('IN_OPERATION', 'В работе'),
+                                                           ('UNDER_REPAIR', 'На ремонте'),
+                                                           ('IDLE', 'Простой'),
+                                                           ('DECOMMISSIONED', 'Списана');
