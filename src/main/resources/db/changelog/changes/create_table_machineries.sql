a5dd1babd482536948686f2e6b7d81c39f4fa7be
danmv24 <danmn119@gmail.com>
2026-01-08 16:08:03 +0300

    sql script for creating the machineries table

---
+++ src/main/resources/db/changelog/changes/create_table_machineries.sql
@@ -0,0 +1,10 @@
+CREATE TABLE "machineries" (
+    machinery_id SERIAL PRIMARY KEY,
+    machinery_name VARCHAR(100) NOT NULL,
+    machinery_type_id INT NOT NULL,
+    inventory_number INT NOT NULL UNIQUE,
+    license_plate VARCHAR(15) NOT NULL UNIQUE,
+    status_id INT NOT NULL,
+    FOREIGN KEY (machinery_type_id) REFERENCES machinery_types(id),
+    FOREIGN KEY (status_id) REFERENCES status_codes(status_id)
+);
