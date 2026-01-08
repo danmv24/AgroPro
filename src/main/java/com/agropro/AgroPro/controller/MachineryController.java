98d164d79ba3f51770cee4bc4c1b14e0271f29cd
danmv24 <danmn119@gmail.com>
2026-01-08 16:34:54 +0300

    create initial controller structure for machinery management endpoints.

---
+++ src/main/java/com/agropro/AgroPro/controller/MachineryController.java
@@ -0,0 +1,14 @@
+package com.agropro.AgroPro.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/machinery")
+public class MachineryController {
+
+
+
+}
