49aa4b8663ab892d0e2b56ebbde78208449caaab
danmv24 <danmn119@gmail.com>
2026-01-08 16:04:07 +0300

    add @Getter and @RequiredArgsConstructor annotations

--- src/main/java/com/agropro/AgroPro/enums/PaymentType.java
+++ src/main/java/com/agropro/AgroPro/enums/PaymentType.java
@@ -1,16 +1,14 @@
 package com.agropro.AgroPro.enums;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum PaymentType {
     FIXED("Фиксированная"),
     HOURLY("Почасовая");
 
     private final String paymentTypeName;
 
-    PaymentType(String paymentTypeName) {
-        this.paymentTypeName = paymentTypeName;
-    }
-
-    public String getPaymentTypeName() {
-        return paymentTypeName;
-    }
 }
