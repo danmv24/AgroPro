e8c631db1c293d44a1e5a6fc06304c4cedad2c57
danmv24 <danmn119@gmail.com>
2026-01-08 16:09:12 +0300

    sql script for inserting data into the machinery_types table

---
+++ src/main/resources/db/changelog/changes/insert_data_to_machinery_types_table.sql
@@ -0,0 +1,5 @@
+INSERT INTO machinery_types(machinery_name) VALUES ('Трактор'),
+                                                   ('Комбайн'),
+                                                   ('Грузовик'),
+                                                   ('Легковой транспорт'),
+                                                   ('Погрузчик');
