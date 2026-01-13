911bf01bac7823e81b4b5a6eb384e65da33f5635
danmv24 <danmn119@gmail.com>
2026-01-13 18:41:13 +0300

    new endpoints

--- src/main/java/com/agropro/AgroPro/controller/MachineryController.java
+++ src/main/java/com/agropro/AgroPro/controller/MachineryController.java
@@ -1,14 +1,46 @@
 package com.agropro.AgroPro.controller;
 
+import com.agropro.AgroPro.form.MachineryForm;
+import com.agropro.AgroPro.service.MachineryService;
+import com.agropro.AgroPro.service.MachineryTypeService;
+import com.agropro.AgroPro.view.MachineryView;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequiredArgsConstructor
-@RequestMapping("/machinery")
+@RequestMapping("/machineries")
 public class MachineryController {
 
+    private final MachineryTypeService machineryTypeService;
+    private final MachineryService machineryService;
 
+    @GetMapping("/add")
+    public String showAddMachineryForm(Model model) {
+        model.addAttribute("machineryForm", new MachineryForm());
+        model.addAttribute("machineryTypes", machineryTypeService.getAllMachineryTypes());
+        return "machineries/machinery_add";
+    }
+
+    @PostMapping("/add")
+    @ResponseBody
+    public ResponseEntity<Map<String, String>> addMachinery(@RequestBody MachineryForm machineryForm) {
+        machineryService.addMachinery(machineryForm);
+        return ResponseEntity.ok(Map.of("status", "success", "message", "Техника успешно добавлена"));
+    }
+
+    @GetMapping("/list")
+    public String showMachineries(Model model) {
+        List<MachineryView> machineryViews = machineryService.getMachineries();
+        model.addAttribute("machineries", machineryViews);
+
+        return "machineries/machinery_list";
+    }
 
 }
