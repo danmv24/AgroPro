d6d91fc8aca46253ffc06d8a4a48d784639cf2bc
danmv24 <danmn119@gmail.com>
2026-01-13 18:24:46 +0300

    new methods

--- src/main/java/com/agropro/AgroPro/service/MachineryService.java
+++ src/main/java/com/agropro/AgroPro/service/MachineryService.java
@@ -1,4 +1,14 @@
 package com.agropro.AgroPro.service;
 
+import com.agropro.AgroPro.form.MachineryForm;
+import com.agropro.AgroPro.view.MachineryView;
+
+import java.util.List;
+
 public interface MachineryService {
+
+    void addMachinery(MachineryForm machineryForm);
+
+    List<MachineryView> getMachineries();
+
 }
--- src/main/java/com/agropro/AgroPro/service/impl/DefaultMachineryService.java
+++ src/main/java/com/agropro/AgroPro/service/impl/DefaultMachineryService.java
@@ -1,13 +1,38 @@
 package com.agropro.AgroPro.service.impl;
 
+import com.agropro.AgroPro.form.MachineryForm;
+import com.agropro.AgroPro.mapper.MachineryMapper;
+import com.agropro.AgroPro.repository.MachineryRepository;
 import com.agropro.AgroPro.service.MachineryService;
+import com.agropro.AgroPro.service.MachineryTypeService;
+import com.agropro.AgroPro.service.StatusService;
+import com.agropro.AgroPro.view.MachineryView;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class DefaultMachineryService implements MachineryService {
 
+    private final MachineryRepository machineryRepository;
 
+    private final MachineryTypeService machineryTypeService;
+
+    private final StatusService statusService;
+
+    @Override
+    public void addMachinery(MachineryForm machineryForm) {
+        machineryTypeService.validateExists(machineryForm.getMachineryTypeId());
+        Long idleStatusId = statusService.getIdleStatusCodeId();
+
+        machineryRepository.save(MachineryMapper.toModel(machineryForm, idleStatusId));
+    }
+
+    @Override
+    public List<MachineryView> getMachineries() {
+        return machineryRepository.findAll();
+    }
 
 }
