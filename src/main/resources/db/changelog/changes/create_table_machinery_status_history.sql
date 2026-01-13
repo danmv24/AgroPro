edfdd765283d16cd408d7b347669e7ca72c761ff
danmv24 <danmn119@gmail.com>
2026-01-13 18:02:55 +0300

    sql script for creating a table with a history of status changes for equipment

---
+++ src/main/resources/db/changelog/changes/create_table_machinery_status_history.sql
@@ -0,0 +1,8 @@
+CREATE TABLE "machinery_status_history" (
+    id BIGSERIAL PRIMARY KEY,
+    machinery_id BIGINT NOT NULL,
+    status_id INT NOT NULL,
+    changed_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
+    FOREIGN KEY (machinery_id) REFERENCES machineries(machinery_id),
+    FOREIGN KEY (status_id) REFERENCES status_codes(status_id)
+);
