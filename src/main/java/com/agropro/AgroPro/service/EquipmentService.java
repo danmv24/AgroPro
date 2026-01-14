3b360307ac34374ebf29fa43c92a0b866acf557d
danmv24 <danmn119@gmail.com>
2026-01-14 16:26:23 +0300

    logic

--- src/main/java/com/agropro/AgroPro/service/EquipmentService.java
+++ src/main/java/com/agropro/AgroPro/service/EquipmentService.java
@@ -1,4 +1,9 @@
 package com.agropro.AgroPro.service;
 
+import com.agropro.AgroPro.form.EquipmentForm;
+
 public interface EquipmentService {
+
+    void addEquipment(EquipmentForm equipmentForm);
+
 }
--- src/main/java/com/agropro/AgroPro/service/impl/DefaultEquipmentService.java
+++ src/main/java/com/agropro/AgroPro/service/impl/DefaultEquipmentService.java
@@ -1,6 +1,11 @@
 package com.agropro.AgroPro.service.impl;
 
+import com.agropro.AgroPro.form.EquipmentForm;
+import com.agropro.AgroPro.mapper.EquipmentMapper;
+import com.agropro.AgroPro.repository.EquipmentRepository;
 import com.agropro.AgroPro.service.EquipmentService;
+import com.agropro.AgroPro.service.EquipmentTypeService;
+import com.agropro.AgroPro.service.StatusService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -8,6 +13,17 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class DefaultEquipmentService implements EquipmentService {
 
+    private final EquipmentRepository equipmentRepository;
 
+    private final EquipmentTypeService equipmentTypeService;
 
+    private final StatusService statusService;
+
+    @Override
+    public void addEquipment(EquipmentForm equipmentForm) {
+        equipmentTypeService.validateExistsById(equipmentForm.getEquipmentTypeId());
+        Long idleStatusId = statusService.getIdleStatusCodeId();
+
+        equipmentRepository.save(EquipmentMapper.toModel(equipmentForm, idleStatusId));
+    }
 }
