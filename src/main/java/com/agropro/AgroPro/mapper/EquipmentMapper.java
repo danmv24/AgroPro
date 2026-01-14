a50abd928020faae150963f47948972ecbfb9dfb
danmv24 <danmn119@gmail.com>
2026-01-14 16:18:56 +0300

    mapping from dto to model

---
+++ src/main/java/com/agropro/AgroPro/mapper/EquipmentMapper.java
@@ -0,0 +1,17 @@
+package com.agropro.AgroPro.mapper;
+
+import com.agropro.AgroPro.form.EquipmentForm;
+import com.agropro.AgroPro.model.Equipment;
+
+public class EquipmentMapper {
+
+    public static Equipment toModel(EquipmentForm equipmentForm, Long idleStatusId) {
+        return Equipment.builder()
+                .equipmentName(equipmentForm.getEquipmentName())
+                .equipmentTypeId(equipmentForm.getEquipmentTypeId())
+                .inventoryNumber(equipmentForm.getInventoryNumber())
+                .statusId(idleStatusId)
+                .build();
+    }
+
+}
