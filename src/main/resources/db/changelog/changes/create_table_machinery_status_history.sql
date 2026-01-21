f86f15abb58c65e3e71f01a47ffaaf214bdff19f
danmv24 <danmn119@gmail.com>
2026-01-21 20:01:54 +0300

    NOW

--- src/main/resources/db/changelog/changes/create_table_machinery_status_history.sql
+++ src/main/resources/db/changelog/changes/create_table_machinery_status_history.sql
@@ -2,7 +2,7 @@ CREATE TABLE "machinery_status_history" (
     id BIGSERIAL PRIMARY KEY,
     machinery_id BIGINT NOT NULL,
     status_id INT NOT NULL,
-    changed_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
+    changed_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
     FOREIGN KEY (machinery_id) REFERENCES machineries(machinery_id),
     FOREIGN KEY (status_id) REFERENCES status_codes(status_id)
 );
