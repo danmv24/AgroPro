cc6927dd67587c8e5b8577905ec7aa38b22eaf70
danmv24 <danmn119@gmail.com>
2026-01-21 20:13:49 +0300

    repository method calls have been replaced with service method calls.

--- src/main/java/com/agropro/AgroPro/controller/FieldPlantingController.java
+++ src/main/java/com/agropro/AgroPro/controller/FieldPlantingController.java
@@ -1,9 +1,9 @@
 package com.agropro.AgroPro.controller;
 
 import com.agropro.AgroPro.form.FieldPlantingForm;
-import com.agropro.AgroPro.repository.CropRepository;
-import com.agropro.AgroPro.repository.FieldRepository;
+import com.agropro.AgroPro.service.CropService;
 import com.agropro.AgroPro.service.FieldPlantingService;
+import com.agropro.AgroPro.service.FieldService;
 import com.agropro.AgroPro.view.FieldPlantingView;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -19,14 +19,14 @@ import java.util.Map;
 @RequestMapping("/field-plantings")
 public class FieldPlantingController {
 
-    private final CropRepository cropRepository;
-    private final FieldRepository fieldRepository;
+    private final CropService cropService;
+    private final FieldService fieldService;
     private final FieldPlantingService fieldPlantingService;
 
     @GetMapping("/add")
     public String showAddFieldPlantingForm(Model model) {
-        model.addAttribute("fields", fieldRepository.findAll());
-        model.addAttribute("crops", cropRepository.findAll());
+        model.addAttribute("fields", fieldService.getAllFields());
+        model.addAttribute("crops", cropService.getAllCrops());
 
         return "fields/field_plantings_add";
     }
