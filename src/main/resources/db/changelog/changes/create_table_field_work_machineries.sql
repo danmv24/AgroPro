d1c78787acb4ad8d1812e035d5557a266f8ac76e
danmv24 <danmn119@gmail.com>
2026-01-28 13:07:56 +0300

    created field_work_machineries table, which stores information about the machineries involved in a particular job.

---
+++ src/main/resources/db/changelog/changes/create_table_field_work_machineries.sql
@@ -0,0 +1,7 @@
+CREATE TABLE "field_work_machineries" (
+    field_work_id BIGINT NOT NULL,
+    machinery_id BIGINT NOT NULL,
+    PRIMARY KEY (field_work_id, machinery_id),
+    FOREIGN KEY (field_work_id) REFERENCES field_works(id) ON DELETE CASCADE,
+    FOREIGN KEY (machinery_id) REFERENCES machineries(machinery_id)
+);
