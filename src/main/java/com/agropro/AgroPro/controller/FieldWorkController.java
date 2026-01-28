372bb5e16f046260adacedbcf83f7bfe3cd17399
danmv24 <danmn119@gmail.com>
2026-01-28 22:03:53 +0300

    added new method for processing the form

--- src/main/java/com/agropro/AgroPro/controller/FieldWorkController.java
+++ src/main/java/com/agropro/AgroPro/controller/FieldWorkController.java
@@ -2,11 +2,17 @@ package com.agropro.AgroPro.controller;
 
 import com.agropro.AgroPro.form.FieldWorkForm;
 import com.agropro.AgroPro.service.*;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Map;
 
 @Controller
 @RequiredArgsConstructor
@@ -18,6 +24,7 @@ public class FieldWorkController {
     private final EquipmentService equipmentService;
     private final FieldService fieldService;
     private final WorkTypeService workTypeService;
+    private final FieldWorkService fieldWorkService;
 
     @GetMapping("/add")
     public String showPlanForm(Model model) {
@@ -29,6 +36,12 @@ public class FieldWorkController {
         model.addAttribute("fieldWorkForm", new FieldWorkForm());
 
         return "field_works/field_work_add";
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<Map<String, String>> planFieldWork(@Valid @RequestBody FieldWorkForm fieldWorkForm) {
+        fieldWorkService.addFieldWork(fieldWorkForm);
+        return ResponseEntity.ok(Map.of("status", "success", "message", "Работа успешно запланирована"));
     }
 
 }
