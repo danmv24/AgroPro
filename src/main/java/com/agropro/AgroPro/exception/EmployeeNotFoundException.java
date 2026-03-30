30346484313e761d465b831a313db904abae25be
danmv24 <danmn119@gmail.com>
2026-03-30 18:07:27 +0300

    deleted unused constructor

--- src/main/java/com/agropro/AgroPro/exception/EmployeeNotFoundException.java
+++ src/main/java/com/agropro/AgroPro/exception/EmployeeNotFoundException.java
@@ -7,20 +7,11 @@ import java.util.Set;
 @Getter
 public class EmployeeNotFoundException extends NotFoundException {
 
-    private final Long employeeId;
-
     private final Set<Long> missingIds;
-
-    public EmployeeNotFoundException(Long employeeId) {
-        super("Сотрудник с id " + employeeId + " не найден");
-        this.employeeId = employeeId;
-        this.missingIds = null;
-    }
 
     public EmployeeNotFoundException(Set<Long> missingIds) {
         super("Не найдены сотрудники со следующими id: " + missingIds);
         this.missingIds = missingIds;
-        this.employeeId = null;
     }
 
 }
